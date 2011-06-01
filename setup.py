5860812e8507b1c6f61a12c2692a26c1f3877a7a
vishnevskiy <vishnevskiy@gmail.com>
2011-06-01 16:06:38 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'Python Library for Blizzard\'s Community Platform API'
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+
+VERSION = '0.1'
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Database',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(name='battlenet',
+      version=VERSION,
+      packages=find_packages(),
+      author='Stanislav Vishnevskiy',
+      author_email='vishnevskiy@gmail.com',
+      url='https://github.com/vishnevskiy/battlenet',
+      license='MIT',
+      include_package_data=True,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      platforms=['any'],
+      classifiers=CLASSIFIERS,
+      test_suite='tests',
+)
