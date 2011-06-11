80bf73b7bfa45c7cf97f320d79ca5ca7179940f7
vishnevskiy <vishnevskiy@gmail.com>
2011-06-12 04:06:17 +0800

    - __class__ is not valid - eu character test - new exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -11,8 +11,12 @@ class EventletTest(unittest.TestCase):
         self.connection = battlenet.Connection(public_key=PUBLIC_KEY, private_key=PRIVATE_KEY)
 
     def test_character_not_found(self):
-        self.assertRaises(battlenet.APIError,
+        self.assertRaises(battlenet.CharacterNotFound,
             lambda: self.connection.get_character(battlenet.UNITED_STATES, 'Fake Realm', 'Fake Character'))
+
+    def test_guild_not_found(self):
+        self.assertRaises(battlenet.GuildNotFound,
+            lambda: self.connection.get_guild(battlenet.UNITED_STATES, 'Fake Realm', 'Fake Guild'))
 
     def test_realm_not_found(self):
         self.assertRaises(battlenet.RealmNotFound, lambda: self.connection.get_realm(battlenet.EUROPE, 'Fake Realm'))
