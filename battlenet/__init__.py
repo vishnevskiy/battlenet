b4df1181d3db60dec3feeb4a15bbd30e7b0dd1cf
paca-project <archivist@Archive-of-Character-History.com>
2011-07-23 16:55:18 -0700

    Fixing a bug present when using this library with Google App Engine (GAE): 'import *' not allowed with 'from .' (__init__.py, line 1)
    When using Google App Engine you're limited to a limited version of Python 2.5.
    There is a bug in 2.5 that wasn't fixed until 2.6 that causes the above error.
    http://mail.python.org/pipermail/python-list/2009-January/1189517.html
    http://bugs.python.org/issue2400
    
    To make this work in GAE you have to manually import everything, rather than "*".
    These imports are one line at a time for readability.

--- battlenet/__init__.py
+++ battlenet/__init__.py
@@ -1,6 +1,44 @@
-from .connection import *
-from .things import *
-from .exceptions import *
-from .utils import *
-from .enums import *
-from .constants import *
+from .connection import Connection
+
+from .constants import UNITED_STATES
+from .constants import EUROPE
+from .constants import KOREA
+from .constants import TAIWAN
+
+from .enums import RACE
+from .enums import CLASS
+from .enums import QUALITY
+from .enums import RACE_TO_FACTION
+
+from .exceptions import APIError
+from .exceptions import CharacterNotFound
+from .exceptions import GuildNotFound
+from .exceptions import RealmNotFound
+
+from .things import Thing
+from .things import LazyThing
+from .things import Character
+from .things import Title
+from .things import Reputation
+from .things import Stats
+from .things import Appearance
+from .things import Equipment
+from .things import Build
+from .things import Glyph
+from .things import Instance
+from .things import Boss
+from .things import Profession
+from .things import Pet
+from .things import Guild
+from .things import Emblem
+from .things import Perk
+from .things import Reward
+from .things import Realm
+from .things import EquippedItem
+from .things import Class
+from .things import Race
+
+from .utils import normalize
+from .utils import quote
+from .utils import make_icon_url
+from .utils import make_connection
